61b03ef31d7d47b7820270cda9658a087c6a45f2
Robbie <txt3rob@gmail.com>
2013-08-29 08:07:12 +0100

    modified:   sms.py

--- sms.py
+++ sms.py
@@ -18,35 +18,11 @@ cursor = connection.cursor ()
 
 def off():
 	lcd.clear()
-	cursor.execute ("UPDATE `texts`  SET forward = '1'")
 	lcd.backlight(lcd.OFF)
 
-def split():
-	lcd.clear()
-	lcd.backlight(lcd.BLUE)
-	lcd.message("  Incoming \n    SMS")
-	sleep(5)
-	cursor.execute ("SELECT * FROM `texts` WHERE `content` IS NOT NULL AND `forward` =0 LIMIT 0 , 10")
-	data = cursor.fetchall ()
-	for row in data :
-		str = row[2]
-
-		MAX = 16
-		pos = 0
-		l = len( str)
-
-		while (pos < l) :
-			stop = str.rfind( " ", pos, pos + MAX + 1 )
-			if ( stop < 0 ) :
-				if (pos + MAX < l) :
-					stop = pos + MAX
-				else :
-					stop = l
-			lcd.clear()
-			lcd.message( str[ pos : stop + 1 ] )
-			
+				
 def display():
-	cursor.execute ("SELECT * FROM `texts` WHERE `content` IS NOT NULL AND `forward` =0 LIMIT 0 , 10")
+	cursor.execute ("SELECT * FROM `texts` WHERE `content` IS NOT NULL AND `messagesent` =0 LIMIT 0 , 10")
 	data = cursor.fetchall ()
 	for row in data :
 		lcd.clear()
@@ -61,6 +37,7 @@ def display():
 		lcd.backlight(lcd.BLUE)
 		lcd.message(" End Of Messages")
 		sleep(5)
+		cursor.execute ("UPDATE `texts`  SET messagesent = '1' WHERE id = %s" % row[0])
 		lcd.clear()
 		
 
